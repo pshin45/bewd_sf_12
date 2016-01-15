bae0ff2061560f1866147c9e0d8efc68de935705
Peter Shin <pshin45@gmail.com>
2016-01-14 20:09:30 -0800

    refreshing repo

--- 13_Authentication/starter_code/Rewsly_Authentication/db/schema.rb
+++ 13_Authentication/starter_code/Rewsly_Authentication/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160112185855) do
+ActiveRecord::Schema.define(version: 20160115030656) do
 
   create_table "categories", force: true do |t|
     t.string   "title"
@@ -30,7 +30,6 @@ ActiveRecord::Schema.define(version: 20160112185855) do
   add_index "categories_stories", ["story_id"], name: "index_categories_stories_on_story_id"
 
   create_table "comments", force: true do |t|
-    t.integer  "user_id"
     t.integer  "story_id"
     t.text     "post"
     t.datetime "created_at"
@@ -38,7 +37,6 @@ ActiveRecord::Schema.define(version: 20160112185855) do
   end
 
   add_index "comments", ["story_id"], name: "index_comments_on_story_id"
-  add_index "comments", ["user_id"], name: "index_comments_on_user_id"
 
   create_table "stories", force: true do |t|
     t.string   "title"
@@ -46,9 +44,25 @@ ActiveRecord::Schema.define(version: 20160112185855) do
     t.integer  "upvotes"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.integer  "user_id"
   end
 
-  add_index "stories", ["user_id"], name: "index_stories_on_user_id"
+  create_table "users", force: true do |t|
+    t.string   "name"
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
 end
