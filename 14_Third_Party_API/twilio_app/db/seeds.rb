699e01c755fea3de1bfe68dbcb1c380d16b35e09
Peter Shin <pshin45@gmail.com>
2016-01-21 17:57:16 -0800

    refreshing repo

---
+++ 14_Third_Party_API/twilio_app/db/seeds.rb
@@ -0,0 +1,9 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# Examples:
+#
+#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create(name: 'Emanuel', city: cities.first)
+Contact.create(name: "Peter", phone: "+13392235655")
+Contact.create(name: "Nari", phone: "+14154709001")
